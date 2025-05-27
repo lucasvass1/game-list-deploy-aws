819980df99ec5f7bc851f4612b56c2c67d91cc97
OtavioAL <otavioauglei@gmail.com>
2025-05-27 14:20:50 -0300

    feat(controllers): added controller for registration

--- game-list-api/src/infra/http/controllers/users-controller.ts
+++ game-list-api/src/infra/http/controllers/users-controller.ts
@@ -1,4 +1,6 @@
+import { UserProps } from '@/domain/entities/user';
 import { AuthenticateUserUseCase } from '@/domain/use-cases/authenticate-user';
+import { RegisterUserUseCase } from '@/domain/use-cases/register-user';
 import { PrismaUsersRepository } from '@/infra/database/prisma/repositories/prisma-users-repository';
 import { generateToken } from '@/utils/jwt';
 
@@ -23,5 +25,18 @@ export class UsersController {
     };
 
     return response;
+  }
+
+  async register(data: UserProps) {
+    const { name, email, password } = data;
+
+    const registerUseCase = new RegisterUserUseCase(this.repository);
+    const { user } = await registerUseCase.execute({ name, email, password });
+    return {
+      name: user.name,
+      email: user.email,
+      id: user.id,
+      createdAt: user.createdAt,
+    };
   }
 }
