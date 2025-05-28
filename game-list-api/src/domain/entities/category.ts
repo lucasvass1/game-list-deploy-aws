a8b1f882eb9990e74b75c8f7e9d69912aa22cfa0
OtavioAL <otavioauglei@gmail.com>
2025-05-27 21:25:31 -0300

    feat(domain): create Category entity

---
+++ game-list-api/src/domain/entities/category.ts
@@ -0,0 +1,36 @@
+export interface CategoryProps {
+  id?: string;
+  title: string;
+  description: string | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class Category {
+  constructor(public props: CategoryProps) {}
+
+  updateCategory(data: Partial<Pick<Category, 'title' | 'description'>>) {
+    if (data.title) this.props.title = data.title;
+    if (data.description) this.props.description = data.description;
+  }
+
+  get id() {
+    return this.props.id;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
