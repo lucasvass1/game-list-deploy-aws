d949eed561043cf77bcb4d2cc7720472cd3819d0
OtavioAL <otavioauglei@gmail.com>
2025-05-27 21:34:29 -0300

    chore(http): add /category route to index.ts

--- game-list-api/src/infra/routes/index.ts
+++ game-list-api/src/infra/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRoutes } from './user-routes';
+import { categoryRoutes } from './category-routes';
 
 const routes = Router();
 
 routes.use('/users', userRoutes);
+routes.use('/category', categoryRoutes);
 
 export { routes };
