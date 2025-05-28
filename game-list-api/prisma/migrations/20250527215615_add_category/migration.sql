2c709feed20bdb3a30895bd27b19b8af58157ba9
OtavioAL <otavioauglei@gmail.com>
2025-05-27 21:25:05 -0300

    feat(migration): create migration to add Category model

---
+++ game-list-api/prisma/migrations/20250527215615_add_category/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "category" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
