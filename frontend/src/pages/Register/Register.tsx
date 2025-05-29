5fb7acddcd867238df47183a4a192b605699bfe1
OtavioAL <otavioauglei@gmail.com>
2025-05-29 20:39:23 -0300

    refactor(Register.jsx): migrate Register page to TypeScript (tsx)

---
+++ frontend/src/pages/Register/Register.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { FormRegister } from "../../components/formRegister/FormRegister.tsx";
+import { Container } from "./styles.ts";
+export function Register() {
+  return (
+    <Container>
+      <FormRegister
+        title="Sign Up"
+        instruction="Register yourself to access the system"
+        login="Already have an account?"
+        linkLogin="/"
+        textLink="Register now"
+      />
+    </Container>
+  );
+}
