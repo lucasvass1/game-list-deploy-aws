5f152bea43360140ffa8a5dab4dc31e0881cfed6
OtavioAL <otavioauglei@gmail.com>
2025-05-30 09:44:48 -0300

    feat: configure ToastContainer for global toast notifications

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./config/ReactQuery/react-query.ts";
-import { AuthProvider } from "./context/AuthContext.tsx";
-import { App } from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './config/ReactQuery/react-query.ts';
+import { AuthProvider } from './context/AuthContext.tsx';
+import { App } from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
           <App />
         </AuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
