00ea1ae386f6059556ad4d5e8aaa276d0261906e
OtavioAL <otavioauglei@gmail.com>
2025-06-01 12:25:42 -0300

    feat(home): add initial structure for useGetUserStats hook

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ContainerPage from '../../components/ContainerPage';
+import { useGetUserStats } from '../../services/users/stats';
+import { useAuth } from '../../context/AuthContext';
 
 export function Home() {
+  const { user } = useAuth();
+  const { data } = useGetUserStats(!!user?.id);
+
+  console.log(data);
+
   return (
     <ContainerPage>
       <div></div>
