5203c6399890c10d89fc450215b98a2a905d89c1
Maiky Caires <maikycaires8@gmail.com>
2025-06-01 17:25:25 -0300

    feat: component header

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,83 @@
+import { useLocation } from 'react-router-dom';
+import {
+  HeaderContainer,
+  Title,
+  Subtitle,
+  Flex,
+  Row,
+  TitleFilter,
+  Button,
+  Input,
+  Select,
+  Actions,
+  SearchButton,
+  ClearButton,
+} from './styles.ts';
+
+interface HeaderProps {
+  userName: string;
+}
+
+export function Header({ userName }: HeaderProps) {
+  const location = useLocation();
+  const path = location.pathname;
+
+  if (path === '/home') {
+    return (
+      <HeaderContainer>
+        <Title>Hello, {userName}!</Title>
+        <Subtitle>Choose one of options below.</Subtitle>
+      </HeaderContainer>
+    );
+  }
+
+  if (path === '/games') {
+    return (
+      <HeaderContainer>
+        <Flex>
+          <Title>Games</Title>
+          <Button>NEW GAME</Button>
+        </Flex>
+
+        <Row>
+          <TitleFilter>Filters</TitleFilter>
+          <Input placeholder="Search Game" />
+          <Select>
+            <option>Select Category</option>
+          </Select>
+          <Select>
+            <option>Filter Favorite</option>
+          </Select>
+          <Actions>
+            <ClearButton>Clear</ClearButton>
+            <SearchButton>Search 🔍</SearchButton>
+          </Actions>
+        </Row>
+      </HeaderContainer>
+    );
+  }
+
+  if (path === '/categories') {
+    return (
+      <HeaderContainer>
+        <Flex>
+          <Title>Categories</Title>
+          <Button>NEW CATEGORY</Button>
+        </Flex>
+      </HeaderContainer>
+    );
+  }
+
+  if (path === '/plataforms') {
+    return (
+      <HeaderContainer>
+        <Flex>
+          <Title>Plataforms</Title>
+          <Button>NEW PLATFORM</Button>
+        </Flex>
+      </HeaderContainer>
+    );
+  }
+
+  return null;
+}
---
+++ frontend/src/components/Header/styles.ts
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  background-color: #121212;
+  padding: 1.5rem 2rem;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  min-height: 8rem;
+`;
+
+export const Title = styled.div`
+  font-size: 1.8rem;
+  font-weight: bold;
+`;
+
+export const Subtitle = styled.div`
+  color: #aaa;
+  font-size: 0.9rem;
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding-bottom: 1rem;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const TitleFilter = styled.label`
+  font-size: 1.2rem;
+`;
+
+export const Button = styled.button`
+  width: 8rem;
+  height: 2.25rem;
+  border-radius: 0.25rem;
+  color: white;
+  background-color: #42d9c8;
+  border: none;
+  cursor: pointer;
+  font-weight: 500;
+`;
+
+export const Input = styled.input`
+  width: 26rem;
+  height: 2.75rem;
+  border-radius: 0.25rem;
+  border: solid 1px #e5e5e5;
+  &::placeholder {
+    padding-left: 0.75rem;
+    color: #c4c4c4;
+  }
+`;
+
+export const Select = styled.select`
+  width: 26rem;
+  height: 2.75rem;
+  border-radius: 0.25rem;
+  border: solid 1px #e5e5e5;
+  color: #c4c4c4;
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const SearchButton = styled.button`
+  background-color: #42d9c8;
+  border: none;
+  padding: 1rem 1.5rem;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 1rem;
+  color: white;
+  font-weight: 500;
+`;
+
+export const ClearButton = styled.button`
+  background-color: #555;
+  color: white;
+  border: none;
+  padding: 1rem 1.5rem;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 1rem;
+`;
